40a9ea6cbf6a32d52bfdcb6ca5c0c4db4af773c7
Pranjal Yadav <85802579+pranjal-y4@users.noreply.github.com>
2024-01-02 20:30:17 +0530

    Update 1_Stereotyping_Discrimination.py

--- pages/1_Stereotyping_Discrimination.py
+++ pages/1_Stereotyping_Discrimination.py
@@ -130,7 +130,7 @@ def main():
     # st.subheader("Our Dataset: stereotyping_dis.csv")
 
     # Load the dataset
-    dataset_path = 'stereotyping_dis.csv'
+    dataset_path = 'pages/stereotyping_dis.csv'
     dataset = load_dataset(dataset_path)
 
             # Load the dataset
